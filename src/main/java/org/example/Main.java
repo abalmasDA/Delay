66763a77f3873a37eaaf7053fce8387ce0dd2b53
abalmasDA <abalmasit@gmail.com>
2023-07-25 20:29:58 +0300

    Commit1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,23 @@
-package org.example;public class Main {
+package org.example;
+
+import java.time.LocalDateTime;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Thread worker1 = new Thread(new Worker("Dmytro"));
+        Thread worker2 = new Thread(new Worker("Oleg"));
+        System.out.println("Потік " + Main.class.getSimpleName() + " стартував роботу - " + LocalDateTime.now());
+        worker1.start();
+        worker2.start();
+
+        try {
+            worker1.join();
+            worker2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Потік " + Main.class.getSimpleName() + " закінчив роботу - " + LocalDateTime.now());
+        System.out.println("Всі потоки завершили роботу");
+
     }
 }
--- src/main/java/org/example/Worker.java
+++ src/main/java/org/example/Worker.java
@@ -1,2 +1,27 @@
-package org.example;public class Worker {
+package org.example;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+public class Worker implements Runnable {
+    private String nameWorker;
+
+    public Worker(String nameWorker) {
+        this.nameWorker = nameWorker;
+    }
+
+    public String getNameWorker() {
+        return nameWorker;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Потік " + getNameWorker() + " стартував роботу - " + LocalDateTime.now().toString());
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Потік " + getNameWorker() + " закінчив роботу - " + LocalDateTime.now().toString());
+    }
 }
